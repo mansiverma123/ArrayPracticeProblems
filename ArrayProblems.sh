8ebda68044e3689a9b2cede3bacf99f5fdfb391a
mansiverma123 <vmansi144@gmail.com>
2021-11-05 16:36:22 +0530

    Added:Second largest and Second smallest element of an array

--- ArrayProblems.sh
+++ ArrayProblems.sh
@@ -11,3 +11,42 @@ do
 done
 
 echo ${RandomList[@]}
+
+max=${RandomList[0]}
+min=${RandomList[0]}
+
+for i in "${RandomList[@]}"
+do
+    if [[ "$i" -gt "$max" ]]; then
+        max="$i"
+    fi
+
+    if [[ "$i" -lt "$min" ]]; then
+        min="$i"
+    fi
+done
+
+echo "Max is: $max"
+echo "Min is: $min"
+
+secondLargest=${RandomList[0]}
+secondSmallest=${RandomList[0]}
+
+for i in "${RandomList[@]}"
+do
+	if [[ "$i" -lt "$secondSmallest" && "$i" -ne "$min" ]];
+	then
+			secondSmallest="$i"
+	fi
+
+   if [[ "$i" -gt "$secondLargest" && "$i" -ne "$max" ]];
+	then
+			secondLargest="$i"
+	fi
+done
+
+echo "Second Largest: $secondLargest"
+echo "Second Smallest: $secondSmallest"
+
+
+
